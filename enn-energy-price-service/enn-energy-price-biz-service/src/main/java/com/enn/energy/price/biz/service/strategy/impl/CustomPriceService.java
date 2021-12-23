d16b6070629ee4b229d280574a4a7ad6ea6f4802
wuchaon <wuchaon@enn.cn>
2021-12-23 11:09:32 +0800

    罗森电价

--- enn-energy-price-service/enn-energy-price-biz-service/src/main/java/com/enn/energy/price/biz/service/strategy/impl/CustomPriceService.java
+++ enn-energy-price-service/enn-energy-price-biz-service/src/main/java/com/enn/energy/price/biz/service/strategy/impl/CustomPriceService.java
@@ -1,19 +1,15 @@
 package com.enn.energy.price.biz.service.strategy.impl;
 
-import java.lang.reflect.InvocationTargetException;
-
-import org.apache.commons.beanutils.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.enn.energy.price.biz.service.strategy.PriceStrategyService;
 import com.enn.energy.price.client.dto.request.ElectricityPriceValueReqDTO;
 import com.enn.energy.price.client.dto.request.EletricityUnifiedReqDto;
 import com.enn.energy.price.client.dto.response.ElectricityPriceUnifiedDetailRespDto;
 import com.enn.energy.price.client.dto.response.ElectricityPriceValueDetailRespDTO;
 import com.enn.energy.price.client.service.ElectricityPriceSelectService;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import top.rdfa.framework.biz.ro.RdfaResult;
 
 /**
@@ -44,16 +40,16 @@ public class CustomPriceService implements PriceStrategyService {
 		ElectricityPriceUnifiedDetailRespDto electricityPriceUnifiedDetailRespDto = new ElectricityPriceUnifiedDetailRespDto();
 		RdfaResult<ElectricityPriceUnifiedDetailRespDto> rdfaResult = new RdfaResult<ElectricityPriceUnifiedDetailRespDto>();
 
-		try {
+	//	try {
 			BeanUtils.copyProperties(electricityPriceUnifiedDetailRespDto, electricityPriceValueDetailRespDTO);
 			rdfaResult.setCode(sepecialResult.getCode());
 			rdfaResult.setMessage(sepecialResult.getMessage());
 			rdfaResult.setData(electricityPriceUnifiedDetailRespDto);
 			rdfaResult.success(sepecialResult.isSuccess());
 
-		} catch (IllegalAccessException | InvocationTargetException e) {
-			log.error("copy properties error:", e);
-		}
+//		} catch (IllegalAccessException | InvocationTargetException e) {
+//			log.error("copy properties error:", e);
+//		}
 		
 
 		return rdfaResult;
--- enn-energy-price-service/enn-energy-price-core-service/src/main/java/com/enn/energy/price/core/service/impl/RdfaProducerDemo.java
+++ enn-energy-price-service/enn-energy-price-core-service/src/main/java/com/enn/energy/price/core/service/impl/RdfaProducerDemo.java
@@ -1,32 +1,32 @@
-package com.enn.energy.price.core.service.impl;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import top.rdfa.framework.mq.RdfaMqClient;
-import top.rdfa.framework.mq.message.RdfaMqResult;
-
-import javax.annotation.Resource;
-
-
-@Service
-public class RdfaProducerDemo  {
-
-    private final static Logger logger= LoggerFactory.getLogger(RdfaProducerDemo.class);
-
-    @Resource
-    private RdfaMqClient rdfaMqClient;
-
-    //配置中心读取topic ，自行设置
-    @Value("${rocketmq.topic}")
-    private String topic;
-
-    public void testSend()  {
-        //指定主题、消息体发送，返回SendResult
-        RdfaMqResult sendResult = rdfaMqClient.send(topic,"test rdfa mq convertAndSend 1");
-        logger.info(" test rdfaMqClient convertAndSend sendResult 1 {} ",sendResult.toString());
-    }
-
-}
-
+//package com.enn.energy.price.core.service.impl;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import top.rdfa.framework.mq.RdfaMqClient;
+//import top.rdfa.framework.mq.message.RdfaMqResult;
+//
+//import javax.annotation.Resource;
+//
+//
+//@Service
+//public class RdfaProducerDemo  {
+//
+//    private final static Logger logger= LoggerFactory.getLogger(RdfaProducerDemo.class);
+//
+//    @Resource
+//    private RdfaMqClient rdfaMqClient;
+//
+//    //配置中心读取topic ，自行设置
+//    @Value("${rocketmq.topic}")
+//    private String topic;
+//
+//    public void testSend()  {
+//        //指定主题、消息体发送，返回SendResult
+//        RdfaMqResult sendResult = rdfaMqClient.send(topic,"test rdfa mq convertAndSend 1");
+//        logger.info(" test rdfaMqClient convertAndSend sendResult 1 {} ",sendResult.toString());
+//    }
+//
+//}
+//
