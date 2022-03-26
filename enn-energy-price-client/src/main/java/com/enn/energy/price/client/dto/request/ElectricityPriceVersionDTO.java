5469f36d7601b654241b4c8291788e27befedd05
wuchaon <wuchaon@enn.cn>
2022-03-26 13:53:17 +0800

    必填校验

--- enn-energy-price-client/src/main/java/com/enn/energy/price/client/dto/request/ElectricityPriceVersionDTO.java
+++ enn-energy-price-client/src/main/java/com/enn/energy/price/client/dto/request/ElectricityPriceVersionDTO.java
@@ -8,7 +8,10 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.Valid;
-import javax.validation.constraints.*;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
